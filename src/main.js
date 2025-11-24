563e8fe03e7363bd06447aeaebaa075831a9c21a
KewenSilva <kewen.macedo@gmail.com>
2025-11-23 22:31:57 -0300

    feat: added button of closed

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
-const { app, BrowserWindow, screen } = require('electron');
+const { app, Menu, Tray, BrowserWindow, screen } = require('electron');
 const path = require('path');
+
+let appIcon = null
+app.whenReady().then(() => {
+  appIcon = new Tray('/home/kewen/Documentos/Dev/screenDev/src/logo-major.png');
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'sobrepor todas janelas', type: 'checkbox' },
+    { label: 'Item2', type: 'checkbox' }
+  ])
+
+  contextMenu.items[1].checked = false
+
+  appIcon.setContextMenu(contextMenu)
+})
+
 
 function createWindow() {
 
-  const winWidth = 520;
-  const winHeight = 340;
+  const winWidth = 625;
+  const winHeight = 540;
 
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   const win = new BrowserWindow({
+    icon: '/home/kewen/Documentos/Dev/screenDev/src/logo-major.png',
     width: winWidth,
     height: winHeight,
-    frame: false,              
-    transparent: false,        
+    frame: false,
+    transparent: false,
     resizable: true,
     alwaysOnTop: true,
     backgroundColor: "#000000",
@@ -20,7 +35,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      webviewTag: true,        
+      webviewTag: true,
     }
   });
 
